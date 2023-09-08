151a7edc34a03c4d66f1f9b06a64230867c1dfbc
Kevin Villegas <kevin.villegas1973@gmail.com>
2023-09-08 09:01:50 -0300

    Filter games by platform

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import PlatformSelector from './components/PlatformSelector';
 import { Grid, GridItem, Show } from '@chakra-ui/react';
 import GenreList from './components/GenreList';
 import GameGrid from './components/GameGrid';
+import { Platform } from './hooks/useGames';
 import { Genre } from './hooks/useGenres';
 import NavBar from './components/NavBar';
 import { useState } from 'react';
-import PlatformSelector from './components/PlatformSelector';
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(
+    null
+  );
 
   return (
     <Grid
@@ -32,8 +36,14 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area='main'>
-        <PlatformSelector />
-        <GameGrid selectedGenre={selectedGenre} />
+        <PlatformSelector
+          selectedPlatform={selectedPlatform}
+          onSelectPlatform={(platform) => setSelectedPlatform(platform)}
+        />
+        <GameGrid
+          selectedPlatform={selectedPlatform}
+          selectedGenre={selectedGenre}
+        />
       </GridItem>
     </Grid>
   );
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,16 +1,17 @@
+import useGames, { Platform } from '../hooks/useGames';
 import { Text, SimpleGrid } from '@chakra-ui/react';
 import GameCardContainer from './GameCardContainer';
 import GameCardSkeleton from './GameCardSkeleton';
 import { Genre } from '../hooks/useGenres';
-import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
 
 interface Props {
   selectedGenre: Genre | null;
+  selectedPlatform: Platform | null;
 }
 
-const GameGrid = ({ selectedGenre }: Props) => {
-  const { data, error, isLoading } = useGames(selectedGenre);
+const GameGrid = ({ selectedGenre, selectedPlatform }: Props) => {
+  const { data, error, isLoading } = useGames(selectedGenre, selectedPlatform);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
 
   return (
