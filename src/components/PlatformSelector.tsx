4ab622ce90a57ee8a3e7fa33cebbaa80d804526b
Kevin Villegas <kevin.villegas1973@gmail.com>
2023-09-08 16:43:04 -0300

    Sort the games

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,6 +1,6 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
-import { BsChevronDown } from 'react-icons/bs';
 import usePlatforms from '../hooks/usePlatforms';
+import { BsChevronDown } from 'react-icons/bs';
 import { Platform } from '../hooks/useGames';
 
 interface Props {
