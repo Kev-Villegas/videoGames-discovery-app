81b02034dbf3765c60bcf819f4c8823d25d061e1
Kevin Villegas <kevin.villegas1973@gmail.com>
2023-09-09 12:42:44 -0300

    Clean up the game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -19,7 +19,7 @@ const GameGrid = ({ gameQuery }: Props) => {
 
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={3}
+        spacing={5}
         padding='10px'
       >
         {isLoading &&
